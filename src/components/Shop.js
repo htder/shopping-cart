db5befaffd6c9274894db8c8ef56815a4ef71495
Henry Derbyshire <henry@htderbyshire.com>
2022-02-16 16:52:56 +0000

    Shop cards are now generated from data in data.json

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,24 @@
 import '../App.css';
 import Card from './Card.js';
+import data from '../data.json';
 
 const Shop = () => {
+  const itemCards = data.map(item => {
+    return (
+      <Card 
+        key={item.name}
+        name={item.name}
+        price={item.price}
+        desc={item.desc}
+      />
+    )
+  });
+
   return (
     <div>
       <h1>Shop</h1>
       <div className="card-container">
-        <Card />
-        <Card />
+        {itemCards}
       </div>
     </div>
   );
